61c133c3a6377caabec39a7c4026b75e7746d39e
lidu <git@hebi.fr>
2021-06-03 10:41:13 +0300

    Environment adjustments (urls, views)

--- torquay_hotel/torquay_hotel/urls.py
+++ torquay_hotel/torquay_hotel/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include ('visitors.urls')),
     path('admin/', admin.site.urls),
+    # path('accounts/', include('accounts.urls')),
+    path('visitors/', include('visitors.urls')),
+    # path('staff/', include('staff.urls')),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
