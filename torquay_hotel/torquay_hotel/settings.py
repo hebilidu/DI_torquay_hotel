7f588fdcb0c1fcb071261012f549a156135d6f58
lidu <git@hebi.fr>
2021-06-03 10:33:12 +0300

    modified settings.py

--- torquay_hotel/torquay_hotel/settings.py
+++ torquay_hotel/torquay_hotel/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
+    'bootstrapform',
+    'star_ratings',
+    'visitors',
+    'staff'
 ]
 
 MIDDLEWARE = [
